07c15d2a203637dfbcc93f9a883dbc37c8760ca2
Konstantin <qodunpob@gmail.com>
2026-07-19 11:41:50 +0900

    feature: Add swagger module

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Shift Flow API')
+    .setDescription('Scheduling and shift assignment API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
+  await app.listen(process.env.PORT ?? 3001);
 }
-bootstrap();
+
+void bootstrap();
