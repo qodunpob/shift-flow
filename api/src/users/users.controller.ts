e69e7901075bffbb1968eca4e76ae50aa9297986
Konstantin <qodunpob@gmail.com>
2026-07-22 00:31:53 +0900

    feature: Add filter for users available for shift

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { UsersService } from '@/users/users.service';
+import { FindUsersQueryDto } from '@/users/users.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('users')
@@ -9,7 +10,7 @@ export class UsersController {
   constructor(private readonly users: UsersService) {}
 
   @Get()
-  findAll() {
-    return this.users.findAll();
+  findAll(@Query() query: FindUsersQueryDto) {
+    return this.users.findAll(query);
   }
 }
