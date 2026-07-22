b05f54ad9bf057c83b1f9ef06c9e30ca0ee41b4e
Konstantin <qodunpob@gmail.com>
2026-07-22 14:36:41 +0900

    chore: Move messages to the top level

--- frontend/src/i18n/request.ts
+++ frontend/src/i18n/request.ts
@@ -1,6 +1,6 @@
-import { hasLocale } from "next-intl";
-import { getRequestConfig } from "next-intl/server";
-import { routing } from "@/i18n/routing";
+import { hasLocale } from 'next-intl'
+import { getRequestConfig } from 'next-intl/server'
+import { routing } from '@/i18n/routing'
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale;
@@ -10,6 +10,6 @@ export default getRequestConfig(async ({ requestLocale }) => {
 
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default,
+    messages: (await import(`../../messages/${locale}.json`)).default,
   };
 });
