b8c479702f52df04ec3df04e3da5494d1bb454a5
Konstantin <qodunpob@gmail.com>
2026-07-26 00:40:05 +0900

    feature: Add link to the schedule details page

--- frontend/src/components/schedule-card/ScheduleCard.tsx
+++ frontend/src/components/schedule-card/ScheduleCard.tsx
@@ -6,6 +6,8 @@ import { DateTime } from 'luxon';
 import { dateFormat } from '@/constants/dates';
 import { useLocale } from 'next-intl';
 import { Schedule } from '@/lib/api/types';
+import Link from 'next/link';
+import { routes } from '@/routes';
 
 export interface ScheduleCardProps {
   schedule: Schedule;
@@ -15,20 +17,28 @@ export const ScheduleCard: React.FC<ScheduleCardProps> = ({ schedule }) => {
   const locale = useLocale();
   const formatDate = (date: string) =>
     DateTime.fromISO(date).toFormat(dateFormat(locale).scheduleBoundaryDate);
+
   return (
     <StyledCard>
       <CardContent>
         <FlexBox justifyContent="space-between">
-          <FlexBox alignItems="baseline">
-            <Typography variant="subtitle2" component="div">
-              {formatDate(schedule.startsAt)}
-              {' – '}
-              {formatDate(schedule.endsAt)}
-            </Typography>
-            <Typography variant="h5" component="div">
-              {schedule.label}
-            </Typography>
-          </FlexBox>
+          <Link href={routes.scheduleDetails(schedule.id)}>
+            <FlexBox alignItems="baseline">
+              <Typography
+                variant={schedule.label ? 'subtitle2' : 'h5'}
+                component="div"
+              >
+                {formatDate(schedule.startsAt)}
+                {' – '}
+                {formatDate(schedule.endsAt)}
+              </Typography>
+              {schedule.label && (
+                <Typography variant="h5" component="div">
+                  {schedule.label}
+                </Typography>
+              )}
+            </FlexBox>
+          </Link>
           <ScheduleStatusChip status={schedule.status} />
         </FlexBox>
       </CardContent>
