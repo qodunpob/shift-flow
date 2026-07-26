60c1a3e50d573e469ae3b382920ac11df62dcb57
Konstantin <qodunpob@gmail.com>
2026-07-26 22:29:18 +0900

    feature: Changed view

--- frontend/src/components/time-sheet/ShiftView.tsx
+++ frontend/src/components/time-sheet/ShiftView.tsx
@@ -12,15 +12,12 @@ export interface ShiftViewProps {
 export const ShiftView: React.FC<ShiftViewProps> = ({ timeLabel, shift }) => {
   return (
     <>
-      <Typography variant="subtitle2" component="div">
-        {timeLabel}
-      </Typography>
-      <FlexBox>
-        <Typography variant="body2" component="div">
-          req: {shift.requiredHeadcount}
+      <FlexBox gap={1} alignItems="baseline">
+        <Typography variant="subtitle2" component="div">
+          {timeLabel}
         </Typography>
         <Typography variant="body2" component="div">
-          rem: {shift.spotsRemaining}
+          ({shift.filledCount} / {shift.requiredHeadcount})
         </Typography>
       </FlexBox>
       <FlexBox gap={0.5} sx={{ my: 1, flexDirection: 'row', flexWrap: 'wrap' }}>
