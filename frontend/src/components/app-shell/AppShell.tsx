d124e13a0d72dfbf82dc54957fb0fd6d2b9cbd18
Konstantin <qodunpob@gmail.com>
2026-07-26 10:22:58 +0900

    feat: add a CurrentUserProvider and mount it in AppShell

--- frontend/src/components/app-shell/AppShell.tsx
+++ frontend/src/components/app-shell/AppShell.tsx
@@ -2,8 +2,14 @@ import type { PropsWithChildren } from 'react';
 import React from 'react';
 import { Container } from '@mui/material';
 import { TopBar, TopBarProps } from '@/components/top-bar/TopBar';
+import { CurrentUserProvider } from '@/providers/CurrentUserProvider';
+import { CurrentUser } from '@/lib/api/types';
 
-export interface AppShellProps extends TopBarProps, PropsWithChildren {}
+export interface AppShellProps
+  extends Omit<TopBarProps, 'user'>,
+    PropsWithChildren {
+  user: CurrentUser;
+}
 
 export const AppShell: React.FC<AppShellProps> = ({
   title,
@@ -17,7 +23,7 @@ export const AppShell: React.FC<AppShellProps> = ({
       sx={{ display: 'flex', flexDirection: 'column', my: 8, gap: 4 }}
     >
       <TopBar title={title} user={user} />
-      {children}
+      <CurrentUserProvider user={user}>{children}</CurrentUserProvider>
     </Container>
   );
 };
