918c559af44c101e8ac0c51d5810990f9c1655ae
Konstantin <qodunpob@gmail.com>
2026-07-26 23:47:47 +0900

    feat: Display rejection reason

--- frontend/src/features/schedule-details/ScheduleDetails.tsx
+++ frontend/src/features/schedule-details/ScheduleDetails.tsx
@@ -3,12 +3,14 @@
 import React from 'react';
 import { TimeSheet } from '@/components/time-sheet/TimeSheet';
 import { Schedule, Shift } from '@/lib/api/types';
-import { Typography } from '@mui/material';
+import { Alert, AlertTitle, Typography } from '@mui/material';
 import { scheduleRange } from '@/utils/scheduleRange';
 import { useLocale, useTranslations } from 'next-intl';
 import { ScheduleToolbar } from '@/components/schedule-toolbar/ScheduleToolbar';
 import { FlexBox } from '@/components/box/box';
 import { ScheduleStatusChip } from '@/components/schedule-status-chip/ScheduleStatusChip';
+import { isManager, isMine } from '@/utils/user';
+import { useCurrentUser } from '@/providers/CurrentUserProvider';
 
 export interface ScheduleDetailsProps {
   schedule: Schedule;
@@ -21,6 +23,13 @@ export const ScheduleDetails: React.FC<ScheduleDetailsProps> = ({
 }) => {
   const locale = useLocale();
   const t = useTranslations();
+  const currentUser = useCurrentUser();
+  const isScheduleOwner =
+    isManager(currentUser.roles) && isMine(schedule.createdBy, currentUser.id);
+  const isRejectionReasonVisible =
+    isScheduleOwner &&
+    schedule.status === 'REJECTED' &&
+    schedule.rejectionReason;
 
   return (
     <>
@@ -33,6 +42,12 @@ export const ScheduleDetails: React.FC<ScheduleDetailsProps> = ({
         </Typography>
         <ScheduleStatusChip status={schedule.status} />
       </FlexBox>
+      {isRejectionReasonVisible && (
+        <Alert variant="filled" severity="warning">
+          <AlertTitle>{t('labels.rejectionReason')}</AlertTitle>
+          {schedule.rejectionReason}
+        </Alert>
+      )}
       <ScheduleToolbar schedule={schedule} />
       <TimeSheet schedule={schedule} shifts={shifts} />
     </>
